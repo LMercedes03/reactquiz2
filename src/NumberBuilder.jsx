40b0b1467d33fadbf8d2ae373fd8d0abd466a3cf
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-05-30 10:28:51 -0400

    Complete quiz numberbuilder question

--- src/NumberBuilder.jsx
+++ src/NumberBuilder.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const NumberBuilder = () => {
+    let [counter, setCounter] = useState(0);
+    let [numbers, setNumbers] = useState(" ");
+
+    const onHandleClick = () => {
+        setCounter(++counter)
+        setNumbers(numbers += (counter + " "))
+      }
+      const onHandleReset = () => {
+        setNumbers(" ")
+        setCounter(0)
+      }
 
     return (
-        <>
-            <button>Add Number</button>
-            <button>Reset</button>
-            <br></br>
-            Numbers: 
-        </>
-    )
+    <div className="Main">
+        <button onClick={() => onHandleClick()}>Add Number</button>
+        <button onClick={() => onHandleReset()}>Reset</button>
+        <br></br>
+        Numbers: {numbers}
+    </div>
+  )
 }
